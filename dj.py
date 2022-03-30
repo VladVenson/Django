b8716a525bec5f88606fe65dc964fc9e2b5a5bba
Python <venvlad1@gmail.com>
2022-03-30 16:08:34 +0300

    Users

--- dj.py
+++ dj.py
@@ -1,5 +1,11 @@
 x = 5
 print(x)
 
+# команда создаём суперпользователя
+# python manage.py createsuperuser (vlad, 1)
+
 # команда запуска сервера
 # python manage.py runserver
+
+# команда миграции user
+# python manage.py migrate
